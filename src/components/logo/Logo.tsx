2f1e4518e49c2c9ad77251645c8dfa7e11fde89c
Artem <artembaa8@gmail.com>
2026-08-14 09:58:30 +0300

    fixed imgs2

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -3,7 +3,7 @@ import styles from "./Logo.module.scss"
 const Logo = () => {
     return(
         <div className={styles.logo}>
-            <img src={`${import.meta.env.BASE_URL} public/logo.png`}/>
+            <img src={`${import.meta.env.BASE_URL}logo.png`}/>
             <span className={styles.text}>InvestIQ</span>
         </div>
     )
